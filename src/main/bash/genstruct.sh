4d6edc4f253922e64a6f721c1d639409bcdf7bf8
Chris Wilper <chris@atmire.com>
2019-03-18 07:43:38 -0400

    change order

--- src/main/bash/genstruct.sh
+++ src/main/bash/genstruct.sh
@@ -5,15 +5,15 @@ colors=( Black Blue Brown Green Orange Pink Red Yellow )
 things=( Bird Crayon Elephant Fish Grasshopper Light Lobster Sky )
 
 echo "<import_structure>"
-for i in "${colors[@]}"; do
+for i in "${sizes[@]}"; do
   echo "  <community>"
   echo "    <name>$i</name>"
-  for j in "${things[@]}"; do
+  for j in "${colors[@]}"; do
     echo "    <community>"
     echo "      <name>$i $j</name>"
-    for k in "${sizes[@]}"; do
+    for k in "${things[@]}"; do
       echo "      <collection>"
-      echo "        <name>$k $i $j</name>"
+      echo "        <name>$i $j $k</name>"
       echo "      </collection>"
     done
     echo "    </community>"
